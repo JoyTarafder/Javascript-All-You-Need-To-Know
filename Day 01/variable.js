4267f3e5f3abffc20c23ae3145ad616b189e63a4
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-05-23 00:56:55 +0600

    Refactor for improved readability and consistency

--- Day 01/variable.js
+++ Day 01/variable.js
@@ -1,2 +1,11 @@
-let name = 'Joy Tarafder';
-console.log(name);
+console.log('Hello World'); // String
+console.log('I love Bangladesh'); // String
+console.log('I love programming');  // String
+console.log('I love coding'); // String
+console.log('I love JavaScript' + ' programming'); // Concatenation
+console.log(25 + 99); // Addition
+console.log(129.36); // Float
+console.log('8' + 9); // String
+
+let name = 'Joy Tarafder'; // String
+console.log(name); // String
