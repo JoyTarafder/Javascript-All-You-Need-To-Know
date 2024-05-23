967d15daf90acd2ea91db32c102b48c23c637bda
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-05-23 21:18:44 +0600

    Refactor for improved readability and consistency

---
+++ Day 01/datatypes.js
@@ -0,0 +1,56 @@
+// Data Type
+
+// Primitive Data Types
+// String
+let firstName = 'Joy'; // String
+console.log(firstName); // String
+
+// Number
+let age = 25; // Number
+console.log(age); // Number
+
+// Boolean
+let isMarried = false; // Boolean
+console.log(isMarried); // Boolean
+
+// Undefined
+let lastName; // Undefined
+console.log(lastName); // Undefined
+
+// Null
+let city = null; // Null
+console.log(city); // Null
+
+// Symbol
+let symbol = Symbol('symbol'); // Symbol
+console.log(symbol); // Symbol
+
+// BigInt
+let bigInt = 1234567890123456789012345678901234567890n; // BigInt
+console.log(bigInt); // BigInt
+
+// Reference Data Types
+// Non-Primitive Data Types
+// Array
+let hobbies = ['Reading', 'Coding', 'Traveling']; // Array
+console.log(hobbies); // Array
+
+// Object
+let person = {
+  firstName: 'Joy',
+  age: 25,
+  isMarried: false,
+}; // Object
+console.log(person); // Object
+
+// Function
+let greet = function () {
+  return 'Hello World!';
+}; // Function
+console.log(greet); // Function
+
+// Date
+let today = new Date(); // Date
+console.log(today); // Date
+
+
