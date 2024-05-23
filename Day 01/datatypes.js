98cdc148d825646f921c8a6631a57f103e11737a
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-05-23 21:41:44 +0600

    Refactor code for improved readability and consistency

--- Day 01/datatypes.js
+++ Day 01/datatypes.js
@@ -54,3 +54,22 @@ let today = new Date(); // Date
 console.log(today); // Date
 
 
+// ******************* Number *******************
+let number = 25; // integer
+let number1 = 25.36; // Float
+let number2 = 25e3; // 25000
+let number3 = 25e-3; // 0.025
+let number4 = Number('25'); // 25
+console.log(Number.parseInt(number1)); // 25
+console.log(Number.parseFloat(number1)); // 25
+
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+console.log(Number.MIN_VALUE); // 5e-324
+
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+console.log(Number.MIN_SAFE_INTEGER); // -9007199254740991
+
+console.log(Number.POSITIVE_INFINITY); // Infinity
+console.log(Number.NEGATIVE_INFINITY); // -Infinity
+
+console.log(1/0); // Infinity
