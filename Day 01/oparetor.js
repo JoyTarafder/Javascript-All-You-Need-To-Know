b5fd3f840ff64166f6e891c3a96966f6f60892b6
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-06-01 12:56:17 +0600

    Operator Increment and Post-Increment

--- Day 01/oparetor.js
+++ Day 01/oparetor.js
@@ -1,5 +1,25 @@
 //operator 
 // 1. Arithmetic operator
+// +, -, *, /, %, ++, --
+let a = 10;
+let b = 20;
+//console.log(a % b); // 10
+
+// Increment
+//console.log(a++); // 10
+//console.log(a); // 11
+//console.log(a--); // 10
+//console.log(a); // 9
+
+
+//pre-increment or post-increment
+//console.log(++a); // 10
+//console.log(a); // 10
+//console.log(--a); // 9
+//console.log(a); // 9
+
+
+
 // 2. Assignment operator
 // 3. Comparison operator
 // 4. Logical operator
